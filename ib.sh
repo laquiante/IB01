6ae4b0b2d07ff431eb9da396a6e183820923294d
laquiante <33266194+laquiante@users.noreply.github.com>
2023-08-18 22:22:02 +0200

    Create ib.sh

---
+++ ib.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/bash
+
+# ALQ, Hamburg 2023
+# Assuming an A100 DGX
+# $sudo ./practice7.sh
+
+clear
+
+PS3='Please enter your choice: '
+options=("Cleanup" "Set DGX1 IP for IPoIB" "Set DGX2 IP for IPoIB" "Verify IP" "Quit")
+select opt in "${options[@]}"
+do
+case $opt in
+"Cleanup")
+                 echo "cleaning up the IP(oIB) configuration from the 8 IB cluster interfaces"
+  ./cleanup.sh
+                  break
+                  ;;
+                "Set DGX1 IP for IPoIB")
+                  echo "setting IP(oIB) configuration for DGX1 practice 7"
+                  ./p7-dgx1.sh
+                  break
+                  ;;
+                "Set DGX2 IP for IPoIB")
+                  echo "setting IP(oIB) configuration for DGX2 practice 7"
+                  ./p7-dgx2.sh
+                  break
+                  ;;
+                "Verify IP")
+                  clear
+                  ip -br a
+                  ibdev2netdev
+                  break
+                  ;;
+                "Quit")
+                  break
+                  ;;
+                *) echo "invalid option $REPLY";;
+         esac
+done
